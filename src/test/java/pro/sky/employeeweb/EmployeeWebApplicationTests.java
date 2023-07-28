34a2ab22bb055b3e4dfec0b749d8ad3274a9eb8a
Grisha <guruwolf8@gmail.com>
2023-07-28 22:29:13 +0300

    ППЦ

---
+++ src/test/java/pro/sky/employeeweb/EmployeeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.employeeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
